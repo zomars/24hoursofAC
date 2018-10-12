3c6ffb780526eb7b99b8f7216a531bf0933e2453
zomars <zomars@me.com>
2018-10-11 20:14:00 -0600

    Make it an object

--- ac.js
+++ ac.js
@@ -1,61 +1,69 @@
-var vid = '3xny-xwQ9-k';
-var audio_streams = {};
+function YTAudio(id) {
+  this.id = id;
+  this.audio_streams = {};
+}
 
-// Set up our HTTP request
-var xhr = new XMLHttpRequest();
+YTAudio.prototype.init = function() {
+  // Set up our HTTP request
+  var xhr = new XMLHttpRequest();
 
-// Setup our listener to process compeleted requests
-xhr.onreadystatechange = function() {
-  // Only run if the request is complete
-  if (xhr.readyState !== 4) return;
+  // Setup our listener to process compeleted requests
+  xhr.onreadystatechange = function() {
+    // Only run if the request is complete
+    if (xhr.readyState !== 4) return;
 
-  // Process our return data
-  if (xhr.status === 200) {
-    // What do when the request is successful
-    var data = parse_str(xhr.responseText);
-    var streams = (
-      data.url_encoded_fmt_stream_map +
-      ',' +
-      data.adaptive_fmts
-    ).split(',');
-    console.log(streams);
+    // Process our return data
+    if (xhr.status === 200) {
+      // What do when the request is successful
+      var data = this.parse_str(xhr.responseText);
+      var streams = (
+        data.url_encoded_fmt_stream_map +
+        ',' +
+        data.adaptive_fmts
+      ).split(',');
+      console.log(streams);
 
-    streams.forEach(function(s) {
-      var stream = parse_str(s);
-      var itag = stream.itag * 1;
-      var quality = false;
-      console.log(stream);
-      switch (itag) {
-        case 139:
-          quality = '48kbps';
-          break;
-        case 140:
-          quality = '128kbps';
-          break;
-        case 141:
-          quality = '256kbps';
-          break;
-      }
-      if (quality) audio_streams[quality] = stream.url;
+      streams.forEach(
+        function(s) {
+          var stream = this.parse_str(s);
+          var itag = stream.itag * 1;
+          var quality = false;
+          console.log(stream);
+          switch (itag) {
+            case 139:
+              quality = '48kbps';
+              break;
+            case 140:
+              quality = '128kbps';
+              break;
+            case 141:
+              quality = '256kbps';
+              break;
+          }
+          if (quality) this.audio_streams[quality] = stream.url;
 
-      console.log(audio_streams);
-      document
-        .querySelector('#youtube')
-        .setAttribute('src', audio_streams['128kbps']);
-    });
-  } else {
-    // What do when the request fails
-    console.log('The request failed!');
-  }
+          console.log(this.audio_streams);
+          document
+            .querySelector('#youtube')
+            .setAttribute('src', this.audio_streams['128kbps']);
+        }.bind(this),
+      );
+    } else {
+      // What do when the request fails
+      console.log('The request failed!');
+    }
+  }.bind(this);
+
+  xhr.open(
+    'GET',
+    `https://cors-anywhere.herokuapp.com/https://www.youtube.com/get_video_info?video_id=${
+      this.id
+    }`,
+  );
+  xhr.send();
 };
 
-xhr.open(
-  'GET',
-  `https://cors-anywhere.herokuapp.com/https://www.youtube.com/get_video_info?video_id=${vid}`,
-);
-xhr.send();
-
-function parse_str(str) {
+YTAudio.prototype.parse_str = function(str) {
   return str.split('&').reduce(function(params, param) {
     var paramSplit = param.split('=').map(function(value) {
       return decodeURIComponent(value.replace('+', ' '));
@@ -63,4 +71,4 @@ function parse_str(str) {
     params[paramSplit[0]] = paramSplit[1];
     return params;
   }, {});
-}
+};
